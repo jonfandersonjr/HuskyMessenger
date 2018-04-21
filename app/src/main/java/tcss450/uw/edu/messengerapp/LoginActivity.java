b0ead7c96abc5b4c0a7c57193bf36d27f57151bc
Marshall <marshallfreed@gmail.com>
2018-04-21 16:42:33 -0700

    Trying to force push over master

---
+++ app/src/main/java/tcss450/uw/edu/messengerapp/LoginActivity.java
@@ -0,0 +1,45 @@
+package tcss450.uw.edu.messengerapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity
+        implements LoginFragment.OnLoginFragmentInteractionListener,
+        RegisterFragment.OnRegisterFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        if (savedInstanceState == null) {
+            if (findViewById(R.id.loginFragmentContainer) != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.loginFragmentContainer, new LoginFragment())
+                        .commit();
+            }
+        }
+    }
+
+    @Override
+    public void onRegisterButtonInteraction() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.loginFragmentContainer, new RegisterFragment())
+                .addToBackStack(null).commit();
+    }
+
+    @Override
+    public void onLoginButtonInteraction() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        intent.setFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_NO_HISTORY);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onSubmitButtonInteraction() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        intent.setFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_NO_HISTORY);
+        startActivity(intent);
+    }
+}
